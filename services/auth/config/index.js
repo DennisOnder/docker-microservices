1397f5c3edc1011b589b410a3cd666f7001be339
Denis Onder <webdev.dennisonder@gmail.com>
2019-07-14 15:08:32 +0200

    Environment string in config

--- services/auth/config/index.js
+++ services/auth/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 9001,
-  SECRET_OR_KEY: process.env.SECRET_OR_KEY || "secret"
+  SECRET_OR_KEY: process.env.SECRET_OR_KEY || "secret",
+  ENV: process.env.NODE_ENV || "development"
 };
