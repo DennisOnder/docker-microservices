163993ba8b7d4908f317b2b0a6c9e07af58a803c
Denis Onder <webdev.dennisonder@gmail.com>
2019-07-17 10:44:59 +0200

    Tweaks, dashboard page

--- services/web/router.js
+++ services/web/router.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => res.sendFile(__dirname + "/static/index.html"));
 
+router.get("/dashboard", (req, res) =>
+  res.sendFile(__dirname + "/static/dashboard.html")
+);
+
 module.exports = router;
